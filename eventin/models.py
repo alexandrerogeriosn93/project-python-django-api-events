ffefd9773455f6858069ea8e3158c3bae0499dfc
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-10-25 15:50:35 -0300

    create models Event and Participant

--- eventin/models.py
+++ eventin/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    local = models.CharField(max_length=100)
+    date_event = models.DateTimeField()
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
